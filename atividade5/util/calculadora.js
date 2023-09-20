8a3dfae43aa0e0d735c819bf6e17e51090b31b9a
Victor de Menezes <victormenezesna@gmail.com>
2023-09-19 21:43:12 -0300

    fix: delete files

--- atividade5/util/calculadora.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    somar: (a, b) => Number.parseInt(a) + Number.parseInt(b),
-    subtrair: (a, b) => Number.parseInt(a) - Number.parseInt(b),
-    multiplicar: (a, b) => Number.parseInt(a) * Number.parseInt(b),
-    dividir: (a, b) => Number.parseInt(a) / Number.parseInt(b),
-}
